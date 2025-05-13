c41c7329f5c3e095dd934c0c28aac004d9f371ae
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-13 21:56:29 +0300

    commits

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:8080/api.php')
+    fetch('http://localhost/User-Session-Estimation-System-Project/backend/api.php')
       .then(response => response.json())
       .then(data => {
         console.log(data);
@@ -15,30 +15,12 @@ function App() {
   }, []);
 
   return (
-    <div className="container">
-      <h1>Kullanıcı Login Tahmin Sistemi</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>İsim</th>
-            <th>Son Giriş</th>
-            <th>Ortalama Aralık Tahmini</th>
-            <th>Ertesi Gün Aynı Saat</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{new Date(user.last_login).toLocaleString()}</td>
-              <td>{new Date(user.predictions.average_interval).toLocaleString()}</td>
-              <td>{new Date(user.predictions.next_day_same_time).toLocaleString()}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+    <>
+
+
+
+    </>
+  )
 }
 
-export default App;
+export default App
