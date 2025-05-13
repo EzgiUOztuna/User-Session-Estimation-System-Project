2804c3166d6e3f15cb04011b977f0768a6634442
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-13 23:11:00 +0300

    Update predictions.php

--- backend/predictions.php
+++ backend/predictions.php
@@ -1,17 +1,25 @@
 <?php
-foreach ($rows as $user) {
-    $logins = $user["logins"];
-    $intervals = [];
+$json = file_get_contents("http://localhost/User-Session-Estimation-System-Project/backend/analyze.php");
+$data = json_decode($json, true);
 
-    for ($i = 1; $i < count($logins); $i++) {
-        $t1 = new DateTime($logins[$i - 1]);
-        $t2 = new DateTime($logins[$i]);
-        $intervals[] = $t2->getTimestamp() - $t1->getTimestamp();
+if (isset($data["data"]["rows"])) {
+    $rows = $data["data"]["rows"];
+
+    foreach ($rows as $user) {
+        $logins = $user["logins"];
+        $intervals = [];
+
+        for ($i = 1; $i < count($logins); $i++) {
+            $t1 = new DateTime($logins[$i - 1]);
+            $t2 = new DateTime($logins[$i]);
+            $intervals[] = $t2->getTimestamp() - $t1->getTimestamp();
+        }
+
+        $avgSeconds = array_sum($intervals) / count($intervals);
+        $lastLogin = new DateTime(end($logins));
+        $predictedNext1 = clone $lastLogin;
+        $predictedNext1->modify("+$avgSeconds seconds");
+        echo "<br>";
+        echo "Tahmini login zamanı (Gün + Saat paterni): " . $predictedNext1->format("Y-m-d H:i:s");
     }
-
-    $avgSeconds = array_sum($intervals) / count($intervals);
-    $lastLogin = new DateTime(end($logins));
-    $predictedNext1 = clone $lastLogin;
-    $predictedNext1->modify("+$avgSeconds seconds");
-    echo $predictedNext1;
 }
