3717dd9f77da4fbc75ce597b78537cf691dd2dc6
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-14 10:26:37 +0300

    Update analyze.php

--- backend/analyze.php
+++ backend/analyze.php
@@ -4,7 +4,8 @@ header("Access-Control-Allow-Origin: *"); //Tüm domain'lerden gelen istekleri k
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-$json = file_get_contents("http://localhost/User-Session-Estimation-System-Project/backend/api.php");
+$json = file_get_contents("http://localhost/User-Session-Estimation-System-Project/backend/index
+.php");
 $data = json_decode($json, true);
 $rows = $data["data"]["rows"];
 
