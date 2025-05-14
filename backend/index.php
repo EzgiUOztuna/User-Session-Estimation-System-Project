77addbb1a85ac7e3fcf16545fac1f4041fbaf2a0
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-14 10:25:20 +0300

    Create index.php

---
+++ backend/index.php
@@ -0,0 +1,4 @@
+<?php
+echo "Backend API is running. Try accessing /api.php or /analyze.php";
+//https://user-session-backend.herokuapp.com/analyze.php
+//https://user-session-backend.herokuapp.com/api.php
