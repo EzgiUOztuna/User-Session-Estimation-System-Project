b283102821f52f3f549930380f8cdb6861dda360
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-14 10:26:41 +0300

    Update index.php

--- backend/index.php
+++ backend/index.php
@@ -1,4 +1,14 @@
 <?php
-echo "Backend API is running. Try accessing /api.php or /analyze.php";
-//https://user-session-backend.herokuapp.com/analyze.php
-//https://user-session-backend.herokuapp.com/api.php
+// CORS hataları için başlıklar
+header("Access-Control-Allow-Origin: *"); //Tüm domain'lerden gelen istekleri kabul et
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+//belirtilen adresden JSON verisi al
+$jsonData = file_get_contents('https://case-test-api.humanas.io/');
+//from JSON data to PHP string
+$phpData = json_decode($jsonData, true);
+
+//tarayıcıya JSON formatında geri gönder
+header('Content-Type: application/json');
+echo json_encode($phpData);
