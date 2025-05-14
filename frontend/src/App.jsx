4d7f96e8fa6c5e40ed9efcb8ff850269ded0659e
EzgiUOztuna <ezgiuygn@gmail.com>
2025-05-14 08:16:36 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,20 +5,27 @@ function App() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost/User-Session-Estimation-System-Project/backend/api.php')
+    fetch('http://localhost/User-Session-Estimation-System-Project/backend/analyze.php')
       .then(response => response.json())
       .then(data => {
         console.log(data);
-        setUsers(Array.isArray(data) ? data : []);
+        setUsers(data);
       })
       .catch(error => console.error('Veri çekilirken hata oluştu:', error));
   }, []);
 
   return (
     <>
-
-
-
+      <div className='user-container'>
+        {users.map((user, index) => (
+          <div key={index} className='user-card'>
+            <h3>{user.name}</h3>
+            <p>Son login: {user.lastLogin}</p>
+            <p>Tahmin 1 (Ortalama Aralık): {user.predictionAvgInterval}</p>
+            <p>Tahmin 2 (Patern): {user.predictionPattern}</p>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
